54bda07756e1084a2eb1abf7781c5ce28771898f
Rafael Tavares <tavares.rfl@gmail.com>
2025-10-08 19:32:17 -0300

    feat: add payment entity

---
+++ src/main/java/com/bookstore/bookstore/entities/Payment.java
@@ -0,0 +1,47 @@
+package com.bookstore.bookstore.entities;
+
+import java.time.Instant;
+import java.util.Objects;
+
+public class Payment {
+
+    private Long id;
+    private Instant moment;
+
+    public Payment() {
+    }
+
+    public Payment(Long id, Instant moment) {
+        this.id = id;
+        this.moment = moment;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Payment payment = (Payment) o;
+        return Objects.equals(id, payment.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
