8bbd309544c78ab69dd3c9914b232de7387e2586
Rafael Tavares <tavares.rfl@gmail.com>
2025-10-08 18:59:09 -0300

    feat: add order, orderstatus entities

---
+++ src/main/java/com/bookstore/bookstore/entities/Order.java
@@ -0,0 +1,57 @@
+package com.bookstore.bookstore.entities;
+
+import java.time.Instant;
+import java.util.Objects;
+
+public class Order {
+
+    private Long id;
+    private Instant moment;
+    private StatusOrder status;
+
+    public Order() {
+    }
+
+    public Order(Long id, Instant moment, StatusOrder status) {
+        this.id = id;
+        this.moment = moment;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public StatusOrder getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusOrder status) {
+        this.status = status;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Order order = (Order) o;
+        return Objects.equals(id, order.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
---
+++ src/main/java/com/bookstore/bookstore/entities/StatusOrder.java
@@ -0,0 +1,9 @@
+package com.bookstore.bookstore.entities;
+
+public enum StatusOrder {
+    WAITING_PAYMENT,
+    PAID,
+    SHIPPED,
+    DELIVERED,
+    CANCELED
+}
