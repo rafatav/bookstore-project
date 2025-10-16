f0bb7fddb3e69674695433603972f8767663adae
Rafael Tavares <tavares.rfl@gmail.com>
2025-10-16 16:48:09 -0300

    feat: add product repository

---
+++ src/main/java/com/bookstore/bookstore/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.bookstore.bookstore.repositories;
+
+import com.bookstore.bookstore.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
